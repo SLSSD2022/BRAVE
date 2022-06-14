a47862212c1d32ae7b656886db36adf501b92862
toma-kazuki <kaz6032kaz@g.ecc.u-tokyo.ac.jp>
2022-06-14 11:50:44 +0900

    Update LIDAR_read.ino

--- test_components/test_1_component/LIDAR/LIDAR_read/LIDAR_read.ino
+++ test_components/test_1_component/LIDAR/LIDAR_read/LIDAR_read.ino
@@ -1,5 +1,8 @@
-boolean read_flag =0;
-int t =0;
+boolean read_flag = 0;
+int t = 0;
+int bytenum = 0;
+int distance = 0;
+  
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(115200);
@@ -17,17 +20,61 @@ void loop() { // run over and over
 
   while (Serial2.available() > 0) {
     byte c = Serial2.read();
-//    Serial.println(c,HEX);
-    if(read_flag == 1){
-      Serial.println(c);
-      read_flag =0;
-    }
-    if(c == 0x59){
-      t += 1;
-    }
-    if(t == 1){
-      read_flag = 1;
-      t = 0;
+    switch(bytenum){
+      case 0://frame header must be 0x59
+//        Serial.print("Byte0:");
+//        Serial.println(c,HEX);
+        if(c == 0x59){
+          bytenum += 1;
+        }
+        break;
+      case 1://frame header must be 0x59
+//        Serial.print("Byte1:");
+//        Serial.println(c,HEX);
+        if(c == 0x59){
+          bytenum += 1;
+        }
+        break;
+      case 2://distance value low 8 bits
+//        Serial.print("Byte2:");
+//        Serial.println(c,HEX);
+        distance = c;
+        bytenum += 1;
+        break;
+      case 3://distance value high 8 bits
+//        Serial.print("Byte3:");
+//        Serial.println(c,HEX);
+        distance = distance + 256*(int)c;
+        Serial.print("distance:");
+        Serial.println(distance);
+        distance = 0;
+        bytenum += 1;
+        break;
+      case 4://strength value low 8 bits
+//        Serial.print("Byte4:");
+//        Serial.println(c,HEX);
+        bytenum += 1;
+        break;
+      case 5://strength value high 8 bits
+//        Serial.print("Byte5:");
+//        Serial.println(c,HEX);
+        bytenum += 1;
+        break;
+      case 6://Temp_L low 8 bits
+//        Serial.print("Byte6:");
+//        Serial.println(c,HEX);
+        bytenum += 1;
+        break;
+      case 7://Temp_H high 8 bits
+//        Serial.print("Byte7:");
+//        Serial.println(c,HEX);
+        bytenum += 1;
+        break;
+      case 8://checksum
+//        Serial.print("Byte8:");
+//        Serial.println(c,HEX);
+        bytenum = 0;
+        break;
     }
   }
 }
